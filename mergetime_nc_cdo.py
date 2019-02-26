2ce361729536602429e08c2dc748924f7452512c
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-02-26 11:23:17 +0100

    cdo merge time

--- mergetime_nc_cdo.py
+++ mergetime_nc_cdo.py
@@ -16,28 +16,32 @@ clone_area = "M02"
 main_output_folder = "/scratch-shared/edwinhs/pcrglobwb2_output_gmd_paper_rerun_201902XX/05min/non-natural_1_fat_node/merged/" + clone_area + "/" 
 # - prepare the directory
 if os.path.exists(main_output_folder) == False: os.makedirs(main_output_folder)
+cmd = "rm -r " + main_output_folder + "/*"
+print(cmd)
+os.system(cmd)
+
 
 # location of netcdf input files
 pcrglobwb_output_folder = "/scratch-shared/edwinhs/pcrglobwb2_output_gmd_paper_rerun_201902XX/05min/non-natural_1_fat_node/"
 # - list of netcdf input files
 pcrglobwb_output_files = [
-#~ 'channelStorage_monthAvg_output.nc',
-#~ 'discharge_monthAvg_output.nc',
-#~ 'dynamicFracWat_monthAvg_output.nc',
-#~ 'precipitation_monthTot_output.nc',
-#~ 'runoff_monthTot_output.nc',
-#~ 'satDegLow_monthAvg_output.nc',
-#~ 'satDegUpp_monthAvg_output.nc',
-#~ 'storGroundwaterFossil_monthAvg_output.nc',
-#~ 'storGroundwater_monthAvg_output.nc',
-#~ 'storLowTotal_monthAvg_output.nc',
-#~ 'storUppTotal_monthAvg_output.nc',
-#~ 'surfaceWaterStorage_monthAvg_output.nc',
-#~ 'temperature_monthAvg_output.nc',
-#~ 'totalActiveStorageThickness_monthAvg_output.nc',
-#~ 'totalEvaporation_monthTot_output.nc',
-#~ 'totalRunoff_monthTot_output.nc',
-#~ 'totalWaterStorageThickness_monthAvg_output.nc',
+'channelStorage_monthAvg_output.nc',
+'discharge_monthAvg_output.nc',
+'dynamicFracWat_monthAvg_output.nc',
+'precipitation_monthTot_output.nc',
+'runoff_monthTot_output.nc',
+'satDegLow_monthAvg_output.nc',
+'satDegUpp_monthAvg_output.nc',
+'storGroundwaterFossil_monthAvg_output.nc',
+'storGroundwater_monthAvg_output.nc',
+'storLowTotal_monthAvg_output.nc',
+'storUppTotal_monthAvg_output.nc',
+'surfaceWaterStorage_monthAvg_output.nc',
+'temperature_monthAvg_output.nc',
+'totalActiveStorageThickness_monthAvg_output.nc',
+'totalEvaporation_monthTot_output.nc',
+'totalRunoff_monthTot_output.nc',
+'totalWaterStorageThickness_monthAvg_output.nc',
 'referencePotET_monthTot_output.nc',
 'waterBodyActEvaporation_monthTot_output.nc',
 'waterBodyPotEvaporation_monthTot_output.nc',
